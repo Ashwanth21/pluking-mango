4e82eaf86081852dd0fa4a05534763aa71ca8bb1
Ashwanth21 <74642906+Ashwanth21@users.noreply.github.com>
2021-03-01 17:30:45 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,65 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+const Constraint=Matter.Constraint;
+var treeObj, stoneObj,groundObject, launcherObject;
+var mango1;
+var world,boy;
+
+function preload(){
+	boy=loadImage("images/boy.png");
+  }
+
+function setup() {
+	createCanvas(1300, 600);
+	engine = Engine.create();
+	world = engine.world;
+
+	mango1=new mango(1100,100,30);
+
+	treeObj=new tree(1050,580);
+	groundObject=new ground(width/2,600,width,20);
+
+	stoneObj=new Stone(235,420,30)
+	slingshot=new launcher(stoneObj.body,{x:235,y:420})
+	
+	Engine.run(engine);
+
+}
+
+function draw() {
+
+  background(230);
+  //Add code for displaying text here!
+  image(boy ,200,340,200,300);
+  
+
+  treeObj.display();
+  mango1.display();
+  stoneObj.display();
+  slingshot.display();
+
+  groundObject.display();
+}
+
+
+function mouseDragged()
+{
+	Matter.Body.setPosition(stoneObj.body, {x:mouseX, y:mouseY}) 
+}
+
+function mouseReleased()
+{
+	slingshot.fly();
+    // distance=int(dist(stoneObj.x,stoneObj.y,mango1.x,mango1.y));
+}
+
+function keyPressed() {
+	if (keyCode === 32) {
+    Matter.Body.setPosition(stoneObj.body, {x:235, y:420}) 
+	  slingshot.attach(stoneObj.body);
+	}
+  }
